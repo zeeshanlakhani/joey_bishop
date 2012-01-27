0f811dc1b4642901e4cfc5591008ffaa35e1d60c
Zeeshan Lakhani <zeeshan.lakhani@gmail.com>
2012-01-26 19:52:34 -0500

    requirejs in, more to do

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,11 @@
-require(["jquery", "jquery.alpha", "jquery.beta"], function($) {
-    //the jquery.alpha.js and jquery.beta.js plugins have been loaded.
+require.config({
+	paths: {
+		"jquery": "//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min"
+	}
+});
+
+require(["jquery", "/js/libs/modernizr-2.0.6.js", "plugins", "/js/libs/less-1.2.1.min.js"], function($) {
     $(function() {
-        $('body').alpha().beta();
+    	log("foo");
     });
 });
